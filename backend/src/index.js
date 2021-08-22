800de6bac2206406ee58f6dac58c9eabfedddf96
Sroliver202023 <sroliver202023@gmail.com>
2021-08-22 18:10:16 -0300

    back-end finalizado

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,8 +7,8 @@ const app = express();
 
 app.use(cors());
 // app.use(cors({
-//     origin:'http://meusite.com'
-// }))
+//     origin:'http://localhost:3000'
+// }));
 app.use(express.json());
 app.use(routes);
 app.listen(3333,()=>{
